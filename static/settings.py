0aca556119bd45fe1939036c8e99e29f76870b7f
WarrenWang85 <warren.wang85@gmail.com>
2019-04-21 11:38:44 +0800

    add app blog

--- static/settings.py
+++ static/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-       os.path.join(BASE_DIR, 'portfolio/static/') 
+       os.path.join(BASE_DIR, 'portfolio') 
 ]
 
 STATIC_URL = '/static/'
